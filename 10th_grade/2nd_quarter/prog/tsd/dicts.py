734923bb8518ed3d47658c99a59ebcf81d85296e
leterDieu <leterdieu@proton.me>
2024-11-19 22:33:50 +0300

    dicts --> mb last

--- 10th_grade/2nd_quarter/prog/tsd/dicts.py
+++ 10th_grade/2nd_quarter/prog/tsd/dicts.py
@@ -1,30 +1,26 @@
 from __future__ import annotations
 from collections.abc import Iterable
-from sets import Set
 
 
-class Dict[T]:
-    collection: Iterable[T]
+class Dict[K, V]:
+    collection: Iterable[tuple[K, V]]
 
-    def __init__(self, collection: Iterable[Set[T]]) -> None:
-        try:
-            self.keys = set([i[0] for i in collection])
-            self.values = set([i[1] for i in collection])
-        except IndexError:
-            # perhaps one should add a logger here
-            pass
-        return None
+    def __init__(self, collection: Iterable[tuple[K, V]]) -> None:
+        self.collection = set(collection)
+        self.shadow_set = set(self.collection)
+        self.keys = [i[0] for i in self.shadow_set]
+        self.values = [i[1] for i in self.shadow_set]
 
     def __str__(self) -> str:
-        return ', '.join([f'{key}: {value}' for key, value in zip(self.keys, self.values)])
+        return ', '.join([f'{key}: {value}' for key, value in self.shadow_set])
 
-    def __in__(self, elem: T) -> bool:
+    def __in__(self, elem: V) -> bool:
         return elem in self.values
 
-    def __contains__(self, elem: T) -> bool:
+    def __contains__(self, elem: V) -> bool:
         return elem in self.values
 
-    def __getitem__(self, key: T) -> T | None:
+    def __getitem__(self, key: K) -> V:
         if key not in self.keys:
-            return None
+            raise KeyError
         return list(self.values)[list(self.keys).index(key)]
