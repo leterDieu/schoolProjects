93e7bd22016f9bef59832ae9a2b0b7c5e800dc56
leterDieu <leterdieu@proton.me>
2025-02-01 17:53:02 +0300

    sql 4 (2nd commit)

--- 10th_grade/3rd_quarter/prog/sqlite/sqlite_4/7.sql
+++ 10th_grade/3rd_quarter/prog/sqlite/sqlite_4/7.sql
@@ -1,1 +1,39 @@
---
+WITH
+    genre_count_CTE (actor, genre_count) AS (
+        SELECT
+            p.name AS actor,
+            1 + LENGTH (GROUP_CONCAT (DISTINCT gt.genre_name)) - LENGTH (
+                REPLACE (GROUP_CONCAT (DISTINCT gt.genre_name), ',', '')
+            ) AS genre_count
+        FROM
+            titles AS t
+            LEFT JOIN crew AS c ON t.title_id = c.title_id
+            LEFT JOIN people AS p ON c.person_id = p.person_id
+            LEFT JOIN film_genres AS fg ON t.title_id = fg.title_id
+            LEFT JOIN genre_types AS gt ON fg.genre_id = gt.id
+        GROUP BY
+            p.name
+        HAVING
+            genre_count > 10
+    )
+SELECT
+    t2.title,
+    t2.premiered,
+    r2.rating,
+    GROUP_CONCAT (DISTINCT gt2.genre_name),
+    COUNT(DISTINCT actor) AS actors_count,
+    GROUP_CONCAT (DISTINCT actor)
+FROM
+    genre_count_CTE
+    LEFT JOIN people AS p2 ON genre_count_CTE.actor = p2.name
+    LEFT JOIN crew AS c2 ON p2.person_id = c2.person_id
+    LEFT JOIN titles AS t2 ON c2.title_id = t2.title_id
+    LEFT JOIN rating AS r2 ON t2.title_id = r2.title_id
+    LEFT JOIN film_genres AS fg2 ON t2.title_id = fg2.title_id
+    LEFT JOIN genre_types AS gt2 ON fg2.genre_id = gt2.id
+GROUP BY
+    t2.title
+HAVING
+    actors_count >= 2
+ORDER BY
+    actors_count;
