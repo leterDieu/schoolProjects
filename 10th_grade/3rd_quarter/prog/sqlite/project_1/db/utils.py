0f5636fe1e5afebce3839ee4e64b8f4781591ffe
Nikiforov Alexander <99505176+leterDieu@users.noreply.github.com>
2025-02-15 12:52:44 +0300

    Update utils.py

--- 10th_grade/3rd_quarter/prog/sqlite/project_1/db/utils.py
+++ 10th_grade/3rd_quarter/prog/sqlite/project_1/db/utils.py
@@ -1,6 +1,12 @@
+'''
+utils
+'''
 import os
 
 
 def get_script_from_file(filename: str) -> str:
-    with open(os.path.join("./db", "scripts", filename), "r") as f:
+    '''
+    gets script from file
+    '''
+    with open(os.path.join("./db", "scripts", filename), "r", encoding='utf-8') as f:
         return f.read()
